bfaa4689e1c34891f7c21ddb268108f19da10ca1
ganna <gannatarek44@gmail.com>
2025-03-27 20:16:48 +0200

    3rd commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import{AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink,RouterModule],
+  imports: [RouterLink,RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-   emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  signupAuth= new FormGroup({
+    email: new FormControl([],[Validators.required,Validators.email]),
+    password: new FormControl([],[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]),
+})
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
-
+import{AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink, RouterLinkActive, RouterModule],
+  imports: [RouterLink, RouterLinkActive, RouterModule,ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
+  signupAuth= new FormGroup({
+    name: new FormControl([],[Validators.minLength(2),Validators.required,Validators.maxLength(30)]),
+    email: new FormControl([],[Validators.required,Validators.email]),
+    phone: new FormControl([],[Validators.required,Validators.pattern(/\d{3}\s?\d{4}-?\d{4}/)]),
+    password: new FormControl([],[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)]),
+    rePassword: new FormControl([],[Validators.required,Validators.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)])
+},{validators:this.passwordMatchValidator})
+
+passwordMatchValidator(control:AbstractControl){
+ return control.get('password')?.value==control.get('rePassword')?.value?null:{mismatch:true}
+}
 }
